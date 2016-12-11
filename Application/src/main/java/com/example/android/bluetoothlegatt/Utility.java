208ae5574decc36005743e91adcf48abb6a099bc
Adam Cattermole <a.d.cattermole@gmail.com>
2016-12-11 18:22:22 +0000

    Remove old code from utility

--- Application/src/main/java/com/example/android/bluetoothlegatt/Utility.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/Utility.java
@@ -10,17 +10,9 @@ public class Utility {
 
     public static short shortFromLittleEndianBytes(byte[] b) {
         return ByteBuffer.wrap(b).order(ByteOrder.LITTLE_ENDIAN).getShort();
-//        ByteBuffer bb = ByteBuffer.wrap(b);
-//        bb.order(ByteOrder.LITTLE_ENDIAN);
-//        return bb.getShort();
     }
 
     public static byte[] leBytesFromShort(short s) {
         return ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN).putShort(s).array();
-//        ByteBuffer bb = ByteBuffer.allocate(2);
-//        bb.order(ByteOrder.LITTLE_ENDIAN);
-//        bb.asShortBuffer();
-//        bb.pu(s);
-//        return bb.array();
     }
 }
